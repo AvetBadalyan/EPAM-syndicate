dfb14bb648efd78ea92f207ff04b2a25d389a2cf
AvetBadalyan <avetbadalyan@gmail.com>
2025-03-06 19:27:23 +0400

    update post event logic

--- task05/app/lambdas/api_handler/index.js
+++ task05/app/lambdas/api_handler/index.js
@@ -1,8 +1,44 @@
-exports.handler = async (event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
+const AWS = require('aws-sdk');
+const uuid = require('uuid');
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+const DB_Table = process.env.target_table;
+
+exports.handler = async event => {
+  try {
+    const { principalId, content } = event;
+    const freshID = uuid.v4();
+    const freshTime = new Date().toISOString();
+
+    const item = {
+      TableName: DB_Table,
+      Item: {
+        id: freshID,
+        principalId: principalId,
+        createdAt: freshTime,
+        body: content,
+      },
     };
-    return response;
+
+    await dynamoDb.put(item).promise();
+
+    const resultObj = {
+      id: freshID,
+      principalId: principalId,
+      createdAt: freshTime,
+      body: content,
+    };
+
+    return {
+      statusCode: 201,
+      body: JSON.stringify({ event: resultObj }),
+    };
+  } catch (error) {
+    console.error('Error processing request:', error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Internal Server Error' }),
+    };
+  }
 };
