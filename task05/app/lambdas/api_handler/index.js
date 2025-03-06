16412d270f6a47ee7e100a0fccf1b16274c809c3
AvetBadalyan <avetbadalyan@gmail.com>
2025-03-06 15:21:47 +0400

    updated lambda function and events

--- task05/app/lambdas/api_handler/index.js
+++ task05/app/lambdas/api_handler/index.js
@@ -1,8 +1,23 @@
-exports.handler = async (event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
-    };
-    return response;
+const uuid = require('uuid');
+const AWS = require('aws-sdk');
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+const tableName = process.env.target_table;
+
+exports.handler = async event => {
+  const params = {
+    TableName: tableName,
+    Item: {
+      id: uuid.v4(),
+      principalId: event.principalId,
+      createdAt: new Date().toISOString(),
+      body: event.content,
+    },
+  };
+  try {
+    await dynamoDb.put(params).promise();
+    return { statusCode: 201, event: params.Item };
+  } catch (err) {
+    return JSON.stringify(err, null, 2);
+  }
 };
