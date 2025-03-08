6eabb57a267a9a47aa4624cc2164610fd6303627
AvetBadalyan <avetbadalyan@gmail.com>
2025-03-08 20:08:15 +0400

    function for task08

--- task08/app/lambdas/uuid_generator/index.js
+++ task08/app/lambdas/uuid_generator/index.js
@@ -1,8 +1,43 @@
-exports.handler = async (event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
+const AWS = require('aws-sdk');
+const uuid = require('uuid');
+const s3 = new AWS.S3();
+
+const bucketName = process.env.S3_BUCKET_NAME;
+
+exports.handler = async event => {
+  try {
+    const uuidArray = Array.from({ length: 10 }, () => uuid.v4());
+
+    const timestamp = new Date().toISOString();
+    const fileName = `${timestamp}`;
+
+    const s3Obj = { ids: uuidArray };
+
+    const s3Parameters = {
+      Bucket: bucketName,
+      Key: fileName,
+      Body: JSON.stringify(s3Obj),
+      ContentType: 'application/json',
     };
-    return response;
+
+    const s3BResponse = await s3.putObject(s3Parameters).promise();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: 'uuids successfully generated and uploaded to S3',
+        fileName: fileName,
+        s3BResponse: s3BResponse,
+      }),
+    };
+  } catch (error) {
+    console.log('Error generating and uploading to S3:', error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: 'Failed to generate and upload to S3',
+        error: error.message,
+      }),
+    };
+  }
 };
