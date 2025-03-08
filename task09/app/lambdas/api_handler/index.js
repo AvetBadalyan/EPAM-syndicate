41621eae97e8d54f32eb087a91342222c7855d2f
AvetBadalyan <avetbadalyan@gmail.com>
2025-03-08 22:10:08 +0400

    function for meteo api

--- task09/app/lambdas/api_handler/index.js
+++ task09/app/lambdas/api_handler/index.js
@@ -1,8 +1,21 @@
-exports.handler = async (event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
-    };
-    return response;
+const axios = require('axios');
+
+exports.handler = async event => {
+  const weatherApi =
+    'https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m';
+
+  const response = await axios.get(weatherApi);
+
+  if (event.requestContext.http.path === '/weather') {
+    return JSON.stringify(response.data);
+  }
+
+  return {
+    statusCode: 400,
+    body: JSON.stringify({
+      statusCode: 400,
+      message:
+        'Bad request syntax or unsupported method. Request path: /cmtr-cb055426. HTTP method: GET',
+    }),
+  };
 };
