ff3e97cb1c52f1a3364f7b13e3882c55bc9e0441
AvetBadalyan <avetbadalyan@gmail.com>
2025-03-08 17:38:13 +0400

    task06 update

--- task06/app/lambdas/audit_producer/index.js
+++ task06/app/lambdas/audit_producer/index.js
@@ -1,8 +1,58 @@
-exports.handler = async (event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
-    };
-    return response;
+const AWS = require('aws-sdk');
+const uuid = require('uuid');
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+exports.handler = async event => {
+  const AUDIT_TABLE_NAME = process.env.target_table;
+
+  console.log('event.Records: ', event.Records);
+  for (const item of event.Records) {
+    const eventName = item.eventName;
+    const newImage = AWS.DynamoDB.Converter.unmarshall(item.dynamodb.NewImage);
+    const oldImage = item.dynamodb.OldImage
+      ? AWS.DynamoDB.Converter.unmarshall(item.dynamodb.OldImage)
+      : null;
+
+    if (eventName === 'INSERT') {
+      console.log('INSERT-INSERT');
+
+      const item = {
+        id: uuid.v4(),
+        itemKey: newImage.key,
+        modificationTime: new Date().toISOString(),
+        newValue: {
+          key: newImage.key,
+          value: newImage.value,
+        },
+      };
+
+      await dynamoDb
+        .put({
+          TableName: AUDIT_TABLE_NAME,
+          Item: item,
+        })
+        .promise();
+    } else if (eventName === 'MODIFY') {
+      console.log('MODIFY-MODIFY');
+
+      const item = {
+        id: uuid.v4(),
+        itemKey: newImage.key,
+        modificationTime: new Date().toISOString(),
+        updatedAttribute: 'value',
+        oldValue: oldImage.value,
+        newValue: newImage.value,
+      };
+
+      await dynamoDb
+        .put({
+          TableName: AUDIT_TABLE_NAME,
+          Item: item,
+        })
+        .promise();
+    }
+  }
+
+  return null;
 };
